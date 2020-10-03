cee9078b74565aad36f652074ff6619f510fe183
Maarten Oste <maarten.oste@ugent.be>
2020-10-03 19:08:26 +0200

    added fetch from dato cms

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ recipes }) {
+  console.log(recipes[0].foto);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>My favourite recipes</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {
+          recipes.map((recipe) => (
+            <div className="recipe" key={recipe.id}>
+              <div className="recipe__name">{recipe.recipeName}</div>
+              <img src="https://www.datocms-assets.com/35454/2769679-Pasta with scampi in tomato cream sauce.jpg"></img>
+            </div>
+          ))
+        }
+        
       </main>
 
       <footer className={styles.footer}>
@@ -62,4 +35,18 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const { SiteClient } = require("datocms-client");
+  const client = new SiteClient("76053cf513ef437fff9ac831dabab3");
+  const recipes = await client.items.all({
+      'filter[type]':'323133',
+  });
+  return {
+    props: {
+      recipes: recipes,
+    },
+  }
 }
