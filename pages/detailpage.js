9d32b5faccd48c77c7855295497495d7bf2caa6f
Maarten Oste <maarten.oste@ugent.be>
2020-10-04 14:32:57 +0200

    added detailpages

---
+++ pages/detailpage.js
@@ -0,0 +1,88 @@
+import Head from 'next/head';
+import styles from '../styles/Detailpage.module.css';
+import { useRouter } from 'next/router';
+import { GraphQLClient } from 'graphql-request';
+
+export function request({ query, variables, preview }) {
+	const endpoint = preview
+	  ? `https://graphql.datocms.com/preview`
+	  : `https://graphql.datocms.com/`;
+  
+	const client = new GraphQLClient(endpoint, {
+	  headers: {
+		authorization: `Bearer 76053cf513ef437fff9ac831dabab3`
+	  }
+	});
+	return client.request(query, variables);
+}
+
+export default function DetailPage(recipe) {
+	console.log(recipe);
+	return (
+	  <div className={styles.container}>
+		<Head>
+		<title>{recipe.recipeName}</title>
+		  <link rel="icon" href="/favicon.ico" />
+		</Head>
+  
+		<main className={styles.main}>
+		<div className={styles.recipe__container}>
+			<div className={styles.recipe__name}>{recipe.recipeName}</div>
+				<div className={styles.ingredient__title}>
+					Ingredients
+				</div>
+				<div className={styles.recipe__content}>
+					<div >
+						{
+							recipe.ingredients.map((ingredient) => (
+								<div className={styles.ingredient} key={ingredient.id}>
+									<div className={styles.ingredient__name}>{ingredient.name}</div>
+									<div className={styles.ingredient__amount}>{ingredient.amount}</div>
+								</div>
+							))
+						}
+					</div>
+					<img className={styles.recipe__photo} src={recipe.foto.url}></img>
+				</div>
+				<div className={styles.ingredient__title}>
+					Instructions
+				</div>
+				<div className={styles.recipe__instruction}>
+						{recipe.instruction}
+				</div>
+		</div>
+		</main>
+  
+		<footer className={styles.footer}>
+		  Created by Maarten Oste 
+		</footer>
+	  </div>
+	)
+  }
+
+DetailPage.getInitialProps = async ({query}) => {
+	const DETAILPAGE_QUERY = `query MyQuery {
+		allRecipes(filter: {id: {eq: ${query.id}}}) {
+		  id
+		  recipeName
+		  instruction
+		  ingredients {
+			id
+			name
+			amount
+		  }
+		  foto {
+			id
+			url
+			alt
+		  }
+		}
+	  }`;
+
+	const data = await request({
+		query: DETAILPAGE_QUERY,
+		variables: {}
+	  });
+	const recipe = data.allRecipes[0];
+	return recipe;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { GraphQLClient } from 'graphql-request'
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import { GraphQLClient } from 'graphql-request';
+
+const HOMEPAGE_QUERY = `query MyQuery {
+  allRecipes {
+    id
+    recipeName
+    foto {
+      id
+      url
+      alt
+    }
+  }
+}`;
+
+export function request({ query, variables, preview }) {
+  const endpoint = preview
+    ? `https://graphql.datocms.com/preview`
+    : `https://graphql.datocms.com/`;
+
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer 76053cf513ef437fff9ac831dabab3`
+    }
+  });
+  return client.request(query, variables);
+}
+
+export async function getStaticProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: {}
+  });
+
+  return {
+    props: {
+      recipes: data.allRecipes,
+    },
+  }
+}
 
 export default function Home({ recipes }) {
-  console.log(recipes);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -16,67 +53,20 @@ export default function Home({ recipes }) {
         <div className={styles.recipe__container}>
         {
           recipes.map((recipe) => (
-            <div className={styles.recipe} key={recipe.id}>
-              <img className={styles.recipe__photo} src={recipe.foto.url + "?w=200"} alt={recipe.foto.alt}></img>
-              <div className={styles.recipe__name}>{recipe.recipeName}</div>
-            </div>
+            <Link href={{pathname: '/detailpage', query: {id: recipe.id}}} key={recipe.id}>
+              <div className={styles.recipe} >
+                <img className={styles.recipe__photo} src={recipe.foto.url + "?w=200"} alt={recipe.foto.alt}></img>
+                <div className={styles.recipe__name}>{recipe.recipeName}</div>
+              </div>
+            </Link>
           ))
         }
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Created by Maarten Oste 
       </footer>
     </div>
   )
-}
-const HOMEPAGE_QUERY = `query MyQuery {
-  allRecipes {
-    id
-    recipeName
-    ingredients {
-      id
-      name
-      amount
-    }
-    foto {
-      id
-      url
-      alt
-    }
-  }
-}`
-export function request({ query, variables, preview }) {
-  const endpoint = preview
-    ? `https://graphql.datocms.com/preview`
-    : `https://graphql.datocms.com/`
-
-  const client = new GraphQLClient(endpoint, {
-    headers: {
-      authorization: `Bearer 76053cf513ef437fff9ac831dabab3`
-    }
-  })
-  return client.request(query, variables)
-}
-
-export async function getStaticProps() {
-  const data = await request({
-    query: HOMEPAGE_QUERY,
-    variables: {}
-  });
-
-  console.log(data);
-  return {
-    props: {
-      recipes: data.allRecipes,
-    },
-  }
 }
